a8915382a081bf98bbe538c4c6667609730e169d
Przemysław Kłos <pklos1992@gmail.com>
2025-04-03 23:07:55 +0200

    import '@testing-library/jest-dom'; in TagButton.test

--- src/tests/TagButton.test.tsx
+++ src/tests/TagButton.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import '@testing-library/jest-dom';
 import App from '../App';
 import { PromptObject } from "../types/Prompts";
 import localStorageMock from "../tests/mocks/localStorageMock";
